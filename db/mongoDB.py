207ca89aaf1b5686eb69efc8de2dacbe4252962f
Guido <guidoruggieri97@gmail.com>
2020-08-19 18:47:08 +0200

    Inizialization of database Mongo

---
+++ db/mongoDB.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+class MongoSaver:
+    
+    self.client = MongoClient('mongodb://fruitUser:fruitPass@192.168.1.69:27017/Fruits')
+
+    def write_fruit(fruit):
+        
+
